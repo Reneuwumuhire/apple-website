9cd8828c1cb9fd226c4d76d797a3156407668e1d
Uwumuhire Rene <reneuwumuhire@gmail.com>
2024-03-22 14:22:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+
+//...
+import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+
+Sentry.init({
+	dsn: "https://e07a970dbf41fff14b63755911c4f678@o4506944127565824.ingest.us.sentry.io/4506944129400832",
+	integrations: [
+		Sentry.browserTracingIntegration(),
+		Sentry.replayIntegration({
+			maskAllText: false,
+			blockAllMedia: false,
+		}),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+const transaction = Sentry.startTransaction({ name: "test-transaction" });
+const span = transaction.startChild({ op: "functionX" }); // This function returns a Span
+// functionCallX
+span.finish(); // Remember that only finished spans will be sent with the transaction
+transaction.finish();
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	// return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
+	return (
+		<main className="bg-black">
+			<Navbar />
+			<Hero />
+			<Highlights />
+			<Model />
+			<Features />
+		</main>
+	);
+};
+
+export default Sentry.withProfiler(App);
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,44 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { rightImg, watchImg } from "../utils";
+import VideoCarsousel from "./VideoCarsousel";
+
+function Highlights() {
+	useGSAP(() => {
+		gsap.to("#title", {
+			opacity: 1,
+			y: 0,
+		});
+		gsap.to(".link", {
+			opacity: 1,
+			y: 0,
+			duration: 1,
+			stagger: 0.25,
+		});
+	}, []);
+	return (
+		<section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+			<div className="screen-max-width">
+				<div className=" mb-12 md:flex  w-full items-end justify-between">
+					<h1 id="title" className="section-heading">
+						Get the hightlights.
+					</h1>
+					<div className=" flex flex-wrap items-end gap-5">
+						<p className="link">
+							Watch the film
+							<img src={watchImg} alt="watch" className="ml-2" />
+						</p>
+						<p className="link">
+							Watch the event
+							<img src={rightImg} alt="watch" className="ml-2" />
+						</p>
+					</div>
+				</div>
+				<VideoCarsousel />
+			</div>
+		</section>
+	);
+}
+
+export default Highlights;
